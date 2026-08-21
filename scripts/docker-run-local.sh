ee0538c85704056b3cb1dcfdbe555b8a346350bc
antoni-ostrowski <antoni.ostrowski.dev@gmail.com>
2026-08-22 01:36:15 +0200

    fix(docker): expose port in run-docker-local script

--- scripts/docker-run-local.sh
+++ scripts/docker-run-local.sh
@@ -5,6 +5,7 @@ docker run  \
   -v ./dev/docker-data/:/app/data \
   -v ~/.ssh/id_ed25519:/root/.ssh/id_ed25519:ro \
   -v ~/.ssh/known_hosts:/root/.ssh/known_hosts:ro \
+	-p 3000:3000 \
   --env-file ./scripts/.docker-env \
   antost360/library-syncer:latest
 
